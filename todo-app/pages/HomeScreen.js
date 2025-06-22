89d2712f5e375464923ea556948e679ca3868602
Hanan-Ashraf-Mousa <mosah2312@gmail.com>
2025-06-23 02:33:24 +0300

    refactor HomeScreen component to use Redux for state management and remove unused code

--- todo-app/pages/HomeScreen.js
+++ todo-app/pages/HomeScreen.js
@@ -1,113 +1,36 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { styles } from '../styles.js';
 import TodoForm from '../src/components/TodoForm.js';
 import TodoList from '../src/components/TodoList.js';
-import { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { useEffect } from 'react';
+import Filteration from '../src/components/Filteration.js';
+import { useSelector, useDispatch } from 'react-redux';
+import { saveTodos, fetchTodos } from '../src/components/storage.js'
+import { setTodos } from '../Redux/Slices/todo_slice.js';
 const HomeScreen = () => {
-  // const { navigate } = useNavigation();
-  const [todos, setTodos] = useState([]);
-  const [displayTodos, setDisplayTodos] = useState([]);
-  const [activeFilter, setActiveFilter] = useState('all');
+  const { todos } = useSelector((state) => state.todosState);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const storedTodos = await AsyncStorage.getItem('todos');
-        if (storedTodos) {
-          setTodos(JSON.parse(storedTodos));
-        }
-      } catch (error) {
-        console.error('Error fetching todos from AsyncStorage:', error);
-      }
+    const load = async () => {
+      const todos = await fetchTodos();
+      dispatch(setTodos(todos));
     };
-    fetchTodos();
-  }, []);
 
+    load();
+  }, []);
+  
   useEffect(() => {
-    AsyncStorage.setItem('todos', JSON.stringify(todos));
-    console.log(todos);
+    saveTodos(todos);
   }, [todos]);
 
-  const addTodo = (todo) => {
-    setTodos((prevTodos) => [...prevTodos, todo]);
-  }
-  const removeTodo = (id) => {
-    setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== id));
-  }
-  const completeTodo = (id) => {
-    console.log('Completing todo with id:', id);
-    setTodos((prevTodos) =>
-      prevTodos.map(todo =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  }
-  const filterCompletedTodos = () => {
-    return todos.filter(todo => todo.completed);
-  }
-  const filterActiveTodos = () => {
-    return todos.filter(todo => !todo.completed);
-  }
-  useEffect(() => {
-    if (activeFilter === 'all') {
-      setDisplayTodos(todos);
-    } else if (activeFilter === 'active') {
-      setDisplayTodos(filterActiveTodos());
-    } else if (activeFilter === 'done') {
-      setDisplayTodos(filterCompletedTodos());
-    }
-  }, [todos, activeFilter]);
   return (
     <View style={styles.container}>
       <Text style={styles.headertext}>TODO APP</Text>
-      <TodoForm onSubmit={addTodo} />
-      <View style={styles.dividerLine} />
-      <View style={styles.filterContainer}>
-        <TouchableOpacity
-          style={activeFilter === 'all' ? styles.activeFilterBtn : styles.filterBtn}
-          onPress={() => {
-            setActiveFilter('all');
+      <TodoForm />
+      <Filteration />
 
-          }}
-        >
-          <Text style={activeFilter === 'all' ? styles.text : styles.filterText}>All</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={activeFilter === 'active' ? styles.activeFilterBtn : styles.filterBtn}
-          onPress={() => {
-            setActiveFilter('active');
-
-          }}
-        >
-          <Text style={activeFilter === 'active' ? styles.text : styles.filterText}>Active</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={activeFilter === 'done' ? styles.activeFilterBtn : { ...styles.filterBtn }}
-          onPress={() => {
-            setActiveFilter('done');
-
-          }}
-        >
-          <Text style={activeFilter === 'done' ? styles.text : styles.filterText}>Done</Text>
-        </TouchableOpacity>
-
-      </View>
-      <View style={styles.dividerLine} />
-
-      {displayTodos.length > 0 ? (
-        <TodoList
-          todos={displayTodos}
-          onRemove={removeTodo}
-          onComplete={completeTodo}
-        />
-      ) : (
-        <Text style={{ ...styles.text, color: 'black', fontSize: 16 }}>No tasks available</Text>
-      )}
+      <TodoList />
 
     </View>
   );
